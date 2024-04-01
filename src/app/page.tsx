1c14646b9bf030823bae0a2628c3ccef19964a80
jhowk14 <jonathanperozi16@gmail.com>
2024-04-01 17:01:50 -0300

    customize form with react hook zod and shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,39 @@
 'use client'
 import axios from "axios";
 import { useState } from "react";
-import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Form, FormDescription} from '@/components/ui/form';
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 const schema = z.object({
-  firstName: z.string().min(3),
-  lastName: z.string().min(3),
-  description: z.string().min(10)
+  firstName: z.string().min(3, {
+    message: "Preencha com um nome valido"
+  }),
+  lastName: z.string().min(3, {message: "Preencha com um sobrenome valido"}),
+  description: z.string().min(10, {message: "Preencha com uma Descrição valida"})
 });
-export type schemaType = z.infer<typeof schema>
+
+export type schemaType = z.infer<typeof schema>;
+
 export default function Home() {
   const [pdfUrl, setPdfUrl] = useState('');
-  const { register, handleSubmit, formState: { errors, isSubmitting, isLoading } } = useForm({
+  const form = useForm<schemaType>({
     resolver: zodResolver(schema)
   });
+  const { register, handleSubmit, formState: { errors, isSubmitting, isLoading } } = form
 
-  async function fetchPdf(data: any){
+  async function fetchPdf(data: schemaType) {
     try {
+      // Inicia o estado de carregamento
+      setPdfUrl('');
+      
       const response = await axios.post('/api', data, {
         responseType: 'blob'
       });
-
       // Criar um URL do objeto Blob retornado
       const pdfBlob = new Blob([response.data], { type: 'application/pdf' });
       const pdfUrl = URL.createObjectURL(pdfBlob);
@@ -34,26 +45,43 @@ export default function Home() {
   };
 
   return (
-    <main>
+    <main className="flex justify-center">
       {
-      !pdfUrl && (
-      <>
-      <form onSubmit={handleSubmit(fetchPdf)}>
-        <div className="flex flex-col justify-center items-center mt-20">
-          <input type="text" {...register("firstName")} placeholder="Primeiro Nome" className="p-2 my-1 rounded border" />
-          {errors.firstName && <span className="text-red-500">Este campo é obrigatório</span>}
-          <input type="text" {...register("lastName")} placeholder="Sobrenome" className="p-2 my-1 rounded border" />
-          {errors.lastName && <span className="text-red-500">Este campo é obrigatório</span>}
-          <input type="text" {...register("description")} placeholder="Descrição" className="p-2 my-1 rounded border" />
-          {errors.description && <span className="text-red-500">Este campo é obrigatório</span>}
-          <button type="submit" className="p-2 bg-blue-600 rounded">Gerar PDF</button>
-      {isLoading || isSubmitting && (
-        <><h1>carregando ...</h1></>
-      )}
-        </div>
-      </form>
-      </>
-      )
+        !pdfUrl && (
+          <div className="w-full max-w-4xl p-7">
+          <Form {...form}>
+            <form onSubmit={handleSubmit(fetchPdf)} className="space-y-2">
+              <div>
+                <Label>Nome</Label>
+                <Input placeholder="Fulano" {...register('firstName')} />
+                {errors.firstName && <span className="text-red-500 text-sm inline-block mt-1">
+                  {errors.firstName?.message}
+                </span>}
+              </div>
+              <div>
+                <Label>Sobrenome</Label>
+                <Input placeholder="Ciclano" {...register('lastName')} />
+                {errors.lastName && <span className="text-red-500 text-sm inline-block mt-1">
+                  {errors.lastName?.message}
+                </span>}
+              </div>
+              <div>
+                <Label>Descrição</Label>
+                <Input placeholder="Eu sou ..." {...register('description')} />
+                <FormDescription>
+                  Fale um pouco de sobre você
+                </FormDescription>
+                {errors.description && <span className="text-red-500 text-sm inline-block mt-1">
+                  {errors.description?.message}
+                </span>}
+              </div>
+            <Button type="submit" disabled={isSubmitting || isLoading}>
+              {isSubmitting || isLoading ? "Carregando..." : "Gerar PDF"}
+            </Button>
+            </form>
+          </Form>
+          </div>
+        )
       }
       {pdfUrl && (
         <iframe className="w-screen h-screen" src={pdfUrl} width="100%" height="500px"></iframe>

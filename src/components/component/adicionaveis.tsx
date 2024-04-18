085cf52b69e423599a64c831ac814bf1a1955b10
jhowk14 <jonathanperozi16@gmail.com>
2024-04-18 15:19:36 -0300

    a

--- src/components/component/adicionaveis.tsx
+++ src/components/component/adicionaveis.tsx
@@ -202,6 +202,7 @@ const Adicionaveis = ({ onChange }: { onChange: (a: any) => void }) => {
                   placeholder="Cargo"
                   className="border rounded-md px-2 py-1 mr-2"
                 />
+                <div className="grid sm:grid-cols-2 gap-2">
                   <div className="flex items-center gap-2">
                     <Label htmlFor="from">De</Label>
                     <Input
@@ -231,6 +232,7 @@ const Adicionaveis = ({ onChange }: { onChange: (a: any) => void }) => {
                         disabled={experiencia.trabalhoAtual}
                       />
                   </div>
+                </div>
                 <div className="flex items-center space-x-2">
                   <Checkbox
                     checked={experiencia.trabalhoAtual || false}
